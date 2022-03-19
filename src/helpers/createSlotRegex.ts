918d2de9c2a7d0c0ffaf13af5aa926e0f2369776
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-03-19 12:04:16 +0530

    Break the regex into parts for clarity

--- src/helpers/createSlotRegex.ts
+++ src/helpers/createSlotRegex.ts
@@ -1,4 +1,23 @@
 export function createSlotRegex(slot: string) {
     //  Generate regular expression for the markdown slot
-    return new RegExp('<!--\\s*slot:\\s*' + slot + '\\s*-->\\s*([\\s\\S.]*?)\\s*<!--\\s*\\/slot\\s*-->', 'gim')
+    return new RegExp(
+        [
+            '<!--',             //  Matches <!--
+            '\\s*',             //  Matches zero or more spaces
+            'slot:',            //  Matches slot:
+            '\\s*',
+            slot,               //  Matches the provided slot name
+            '\\s*',
+            '-->',              //  Matches -->
+            '\\s*',
+            '([\\s\\S.]*?)',    //  Matches literally anything
+            '\\s*',
+            '<!--',             // Matches <!--
+            '\\s*',
+            '\\/slot',          //  Matches /slot
+            '\\s*',
+            '-->'               //  Matches -->
+        ].join(''),
+        'gim'
+    )
 }
