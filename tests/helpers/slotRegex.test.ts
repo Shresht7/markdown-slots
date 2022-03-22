03fb2d11367d33d4a07aa508d399a6d07fe4b6c5
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-03-22 12:59:16 +0530

    Add slot regex tests

---
+++ tests/helpers/slotRegex.test.ts
@@ -0,0 +1,66 @@
+//  Library
+import { createSlotRegex } from '../../src/helpers/createSlotRegex'
+
+describe('Slot Regex', () => {
+
+    const regex = createSlotRegex('test')
+
+    it('should create a regular expression', () => {
+        expect(regex).toBeInstanceOf(RegExp)
+    })
+
+    it('should match a slot with the given slot-name', () => {
+        const str = `
+            <!-- slot: test -->
+            <!-- /slot -->
+        `
+        expect(str).toMatch(regex)
+    })
+
+    it('should not match a slot with a different slot-name', () => {
+        const str = `
+            <!-- slot: jest -->
+            <!-- /slot -->
+        `
+        expect(str).not.toMatch(regex)
+    })
+
+    it('should match a slot with default content', () => {
+        const str = `
+            <!-- slot: test -->
+                Default Text
+            <!-- /slot -->
+        `
+        expect(str).toMatch(regex)
+    })
+
+    it('should match inline slots', () => {
+        const str = '<!-- slot: test --><!-- /slot -->'
+        expect(str).toMatch(regex)
+    })
+
+    it('should match slots without spaces', () => {
+        const str = `<!--slot:test--><!--/slot-->`
+        expect(str).toMatch(regex)
+    })
+
+    it('should match slots with additional attributes', () => {
+        const str = `
+            <!-- slot: test prepend="<p align='center'>" append="</p>" -->
+            <!-- /slot -->
+        `
+        expect(str).toMatch(regex)
+    })
+
+    it('should match multiline definitions', () => {
+        const str = `
+            <!--
+                slot: text
+                prepend: "\`\`\`yaml"
+                append: "\`\`\`"
+            -->
+            <!-- /slot -->
+        `
+    })
+
+})
